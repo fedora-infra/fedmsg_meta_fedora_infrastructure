3a7d9daaca4c0f7c5048b870d18b72cae701a75f
Ralph Bean <rbean@redhat.com>
2013-01-31 14:28:08 -0500

    0.0.8

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
 
 setup(
     name='fedmsg_meta_fedora_infrastructure',
-    version='0.0.7',
+    version='0.0.8',
     description=
     "fedmsg metadata providers for Fedora Infrastructure's deployment",
     long_description=long_description,
