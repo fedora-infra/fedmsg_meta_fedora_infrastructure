773578b38f54249e5b6ddba806b5bd8c231291d6
Ralph Bean <rbean@redhat.com>
2013-03-07 12:42:34 -0500

    Secondary arch compose tests.

--- fedmsg_meta_fedora_infrastructure/tests/compose.py
+++ fedmsg_meta_fedora_infrastructure/tests/compose.py
@@ -474,3 +474,243 @@ class TestComposeRawhideRsyncComplete(Base):
             "arch": "",
         },
     }
+
+
+class TestSecondaryArchComposeBranchedComplete(Base):
+    expected_title = "compose.branched.complete (unsigned)"
+    expected_subti = "f18 compose (arm) completed"
+    expected_link = \
+        "https://secondary.fedoraproject.org/pub/fedora-secondary/" + \
+        "development/f18"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.complete",
+        "msg": {
+            "log": "done",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedStart(Base):
+    expected_title = "compose.branched.start (unsigned)"
+    expected_subti = "f18 compose (arm) started"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.start",
+        "msg": {
+            "log": "start",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedMashStart(Base):
+    expected_title = "compose.branched.mash.start (unsigned)"
+    expected_subti = "f18 compose (arm) started mashing"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.mash.start",
+        "msg": {
+            "log": "start",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedMashComplete(Base):
+    expected_title = "compose.branched.mash.complete (unsigned)"
+    expected_subti = "f18 compose (arm) finished mashing"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.mash.complete",
+        "msg": {
+            "log": "done",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedPungifyStart(Base):
+    expected_title = "compose.branched.pungify.start (unsigned)"
+    expected_subti = "started building boot.iso for f18 (arm)"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.pungify.start",
+        "msg": {
+            "log": "start",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedPungifyComplete(Base):
+    expected_title = "compose.branched.pungify.complete (unsigned)"
+    expected_subti = "finished building boot.iso for f18 (arm)"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.pungify.complete",
+        "msg": {
+            "log": "done",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedRsyncStart(Base):
+    expected_title = "compose.branched.rsync.start (unsigned)"
+    expected_subti = "started rsyncing f18 compose (arm) for " + \
+        "public consumption"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.rsync.start",
+        "msg": {
+            "log": "start",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeBranchedRsyncComplete(Base):
+    expected_title = "compose.branched.rsync.complete (unsigned)"
+    expected_subti = \
+        "finished rsync of f18 compose (arm) for public consumption"
+    expected_link = \
+        "https://secondary.fedoraproject.org/pub/fedora-secondary/" + \
+        "development/f18"
+    expected_objects = set(['branched/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.branched.rsync.complete",
+        "msg": {
+            "log": "done",
+            "branch": "f18",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeRawhideComplete(Base):
+    expected_title = "compose.rawhide.complete (unsigned)"
+    expected_subti = "rawhide compose (arm) completed"
+    expected_link = \
+        "https://secondary.fedoraproject.org/pub/fedora-secondary/" + \
+        "development/rawhide"
+    expected_objects = set(['rawhide/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.rawhide.complete",
+        "msg": {
+            "log": "done",
+            "branch": "rawhide",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeRawhideStart(Base):
+    expected_title = "compose.rawhide.start (unsigned)"
+    expected_subti = "rawhide compose (arm) started"
+    expected_objects = set(['rawhide/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.rawhide.start",
+        "msg": {
+            "log": "start",
+            "branch": "rawhide",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeRawhideMashStart(Base):
+    expected_title = "compose.rawhide.mash.start (unsigned)"
+    expected_subti = "rawhide compose (arm) started mashing"
+    expected_objects = set(['rawhide/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.rawhide.mash.start",
+        "msg": {
+            "log": "start",
+            "branch": "rawhide",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeRawhideMashComplete(Base):
+    expected_title = "compose.rawhide.mash.complete (unsigned)"
+    expected_subti = "rawhide compose (arm) finished mashing"
+    expected_objects = set(['rawhide/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.rawhide.mash.complete",
+        "msg": {
+            "log": "done",
+            "branch": "rawhide",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeRawhideRsyncStart(Base):
+    expected_title = "compose.rawhide.rsync.start (unsigned)"
+    expected_subti = "started rsyncing rawhide compose (arm) for " + \
+        "public consumption"
+    expected_objects = set(['rawhide/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.rawhide.rsync.start",
+        "msg": {
+            "log": "start",
+            "branch": "rawhide",
+            "arch": "arm",
+        },
+    }
+
+
+class TestSecondaryArchComposeRawhideRsyncComplete(Base):
+    expected_title = "compose.rawhide.rsync.complete (unsigned)"
+    expected_subti = \
+        "finished rsync of rawhide compose (arm) for public consumption"
+    expected_link = \
+        "https://secondary.fedoraproject.org/pub/fedora-secondary/" + \
+        "development/rawhide"
+    expected_objects = set(['rawhide/arm'])
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.compose.rawhide.rsync.complete",
+        "msg": {
+            "log": "done",
+            "branch": "rawhide",
+            "arch": "arm",
+        },
+    }
