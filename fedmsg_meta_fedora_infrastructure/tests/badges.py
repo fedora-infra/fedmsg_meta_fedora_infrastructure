e0c087f961caefa974ac7cbad679db62fbf9eb29
Ralph Bean <rbean@redhat.com>
2014-05-16 11:54:24 -0400

    Just to be sure.

--- fedmsg_meta_fedora_infrastructure/tests/badges.py
+++ fedmsg_meta_fedora_infrastructure/tests/badges.py
@@ -126,6 +126,7 @@ class TestFirstLogin(Base):
     expected_subti = "ralph logged in to badges.fedoraproject.org " + \
         "for the first time"
     expected_link = "https://badges.fedoraproject.org/user/ralph"
+    expected_icon = "https://apps.fedoraproject.org/img/icons/badges.png"
     expected_secondary_icon = "http://www.gravatar.com/avatar/" + \
         "2f933f4364baaabd2d3ab8f0664faef2?s=64&d=http%3A%2F%2F" + \
         "fedoraproject.org%2Fstatic%2Fimages%2Ffedora_infinity_64x64.png"
