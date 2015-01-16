882c623bd98f9572612899c85bcbbabb91e5f879
Ralph Bean <rbean@redhat.com>
2015-01-15 22:03:21 -0500

    Handle Fedora Atomic ftpsync links.
    Fixes #186.

--- fedmsg_meta_fedora_infrastructure/tests/ftpsync.py
+++ fedmsg_meta_fedora_infrastructure/tests/ftpsync.py
@@ -19,16 +19,38 @@
 #
 """ Tests for ftpsync messages """
 
-import unittest
-
 from fedmsg_meta_fedora_infrastructure.tests.base import Base
 
 from common import add_doc
 
 
+class TestFTPSyncAtomic(Base):
+    """ These messages are published when a new compose of Fedora Atomic
+    is synced out to the master mirror.
+    """
+    expected_title = "bodhi.updates.fedora.sync"
+    expected_subti = "New Fedora 21 atomic content synced out " + \
+        "(60.38M changed with 0 files deleted)"
+    expected_link = \
+        "https://download.fedoraproject.org/pub/fedora/linux/atomic/21/"
+    expected_objects = set(['fedora/atomic/21'])
+
+    msg = {
+        "i": 1,
+        "timestamp": 1344447839.891876,
+        "topic": "org.fedoraproject.prod.bodhi.updates.fedora.sync",
+        "msg": {
+            "bytes": "60.38M",
+            "deleted": "0",
+            "repo": "atomic",
+            "release": "21",
+        },
+    }
+
+
 class TestFTPSyncFedora(Base):
     """ These messages are published when new updates (fresh out of the "mash"
-    process) are synced out to the mirror master.
+    process) are synced out to the master mirror.
 
     Here's an example for the fedora 20 stable repos:
     """
@@ -54,7 +76,7 @@ class TestFTPSyncFedora(Base):
 
 class TestFTPSyncEPELTesting(Base):
     """ These messages are published when new updates (fresh out of the "mash"
-    process) are synced out to the mirror master.
+    process) are synced out to the master mirror.
 
     Here's an example for the epel 6 testing repos:
     """
