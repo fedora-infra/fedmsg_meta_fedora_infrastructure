264d9620d1dcfc5c5ec0a911a3065f98a51a82ce
Ralph Bean <rbean@redhat.com>
2015-10-19 09:26:54 -0400

    New processor for two-week atomic releng messages.

--- fedmsg_meta_fedora_infrastructure/tests/__init__.py
+++ fedmsg_meta_fedora_infrastructure/tests/__init__.py
@@ -58,6 +58,7 @@ from fedmsg_meta_fedora_infrastructure.tests.pagure import *
 from fedmsg_meta_fedora_infrastructure.tests.autocloud import *
 from fedmsg_meta_fedora_infrastructure.tests.infragit import *
 from fedmsg_meta_fedora_infrastructure.tests.taskotron import *
+from fedmsg_meta_fedora_infrastructure.tests.releng import *
 
 from fedmsg_meta_fedora_infrastructure.tests.base import Base
 
