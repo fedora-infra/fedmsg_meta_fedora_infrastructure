ffcda6d6b53f3ce2b2e9afb43bdf883b0138f69d
Ralph Bean <rbean@redhat.com>
2015-12-15 14:45:07 -0500

    Processor and handler for new nagios messages.

--- fedmsg_meta_fedora_infrastructure/tests/__init__.py
+++ fedmsg_meta_fedora_infrastructure/tests/__init__.py
@@ -61,6 +61,7 @@ from fedmsg_meta_fedora_infrastructure.tests.infragit import *
 from fedmsg_meta_fedora_infrastructure.tests.taskotron import *
 from fedmsg_meta_fedora_infrastructure.tests.releng import *
 from fedmsg_meta_fedora_infrastructure.tests.mdapi import *
+from fedmsg_meta_fedora_infrastructure.tests.nagios import *
 
 from fedmsg_meta_fedora_infrastructure.tests.base import Base
 
