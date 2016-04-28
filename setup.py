1bffec85bab9a827fcf9fd752d779bd12c1ffe64
Ralph Bean <rbean@redhat.com>
2016-04-28 09:58:03 -0400

    0.17.4

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ entry_points = {
 
 setup(
     name='fedmsg_meta_fedora_infrastructure',
-    version='0.17.3',
+    version='0.17.4',
     description=
     "fedmsg metadata providers for Fedora Infrastructure's deployment",
     long_description=long_description,
