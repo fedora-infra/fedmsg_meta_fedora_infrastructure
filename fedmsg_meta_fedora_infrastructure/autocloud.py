5de1a63894e1c4d6152a89f65bbeca68327b216a
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-06-30 14:07:32 +0530

    Add/Handle all the old tests as legacy so that it is comaptible with datagrepper

--- fedmsg_meta_fedora_infrastructure/autocloud.py
+++ fedmsg_meta_fedora_infrastructure/autocloud.py
@@ -21,6 +21,7 @@ from fedmsg_meta_fedora_infrastructure import BaseProcessor
 
 
 class AutoCloudProcessor(BaseProcessor):
+
     __name__ = "autocloud"
     __description__ = "Automated Fedora Cloud Image Testing service"
     __link__ = "https://github.com/kushaldas/autocloud"
@@ -28,7 +29,28 @@ class AutoCloudProcessor(BaseProcessor):
     __icon__ = "https://apps.fedoraproject.org/img/icons/fedimg.png"
     __obj__ = "Cloud Image Test"
 
+
+    def _func_router(self, msg, fname, **config):
+        FNAME = 'handle_%s_autocloud_%s'
+
+        if 'compose_id' in msg['msg'] or 'compose_job_id' in msg['msg']:
+            return getattr(self, FNAME % ('new', fname))(msg, **config)
+        else:
+            return getattr(self, FNAME % ('old', fname))(msg, **config)
+
     def subtitle(self, msg, **config):
+        return self._func_router(msg, 'subtitle', **config)
+
+    def secondary_icon(self, msg, **config):
+        return self.__icon__
+
+    def link(self, msg, **config):
+        return self._func_router(msg, 'link', **config)
+
+    def objects(self, msg, **config):
+        return self._func_router(msg, 'objects', **config)
+
+    def handle_new_autocloud_subtitle(self, msg, **config):
         status = msg['msg']['status']
         if 'autocloud.image' in msg['topic']:
             compose_id = msg['msg']['compose_id']
@@ -37,25 +59,25 @@ class AutoCloudProcessor(BaseProcessor):
 
             if status == "queued":
                 tmpl = self._("{image_name}({compose_id}) is {status} for "
-                              "testing")
+                                "testing")
             if status == "running":
                 tmpl = self._("The tests for the {image_name}({compose_id}) "
-                              "have started {status}")
+                                "have started {status}")
             if status == "aborted":
                 tmpl = self._("The tests for the {image_name}({compose_id}) "
-                              "have been {status}")
+                                "have been {status}")
             if status == "failed":
                 tmpl = self._("The tests for the {image_name}({compose_id}) "
-                              "{status}")
+                                "{status}")
             if status == "success":
                 tmpl = self._("The tests for {image_name}({compose_id}) were a"
-                              " {status}")
+                                " {status}")
 
             if release:
                 image_name = "%s (%s)" % (image_name, release)
 
             return tmpl.format(image_name=image_name, status=status,
-                               compose_id=compose_id)
+                                compose_id=compose_id)
 
         if "autocloud.compose" in msg['topic']:
             compose_id = msg['msg']['id']
@@ -69,10 +91,7 @@ class AutoCloudProcessor(BaseProcessor):
 
             return tmpl.format(compose_id=compose_id, status=status)
 
-    def secondary_icon(self, msg, **config):
-        return self.__icon__
-
-    def link(self, msg, **config):
+    def handle_new_autocloud_link(self, msg, **config):
         if 'autocloud.image' in msg['topic']:
             job_id = msg['msg'].get('job_id')
             if job_id:
@@ -86,9 +105,46 @@ class AutoCloudProcessor(BaseProcessor):
             return 'https://apps.fedoraproject.org/autocloud/jobs/%s' % (
                 compose_job_id)
 
-    def objects(self, msg, **config):
+    def handle_new_autocloud_objects(self, msg, **config):
         status = msg['msg']['status']
         if 'autocloud.image' in msg['topic']:
             return set(['autocloud/image/' + status])
         elif 'autocloud.compose' in msg['topic']:
             return set(['autocloud/compose/' + status])
+
+    def handle_old_autocloud_subtitle(self, msg, **config):
+        image_name = msg['msg']['image_name']
+        status = msg['msg']['status']
+        release = msg['msg'].get('release')
+        if 'autocloud.image' in msg['topic']:
+            if status == "queued":
+                tmpl = self._("{image_name} is {status} for testing")
+            if status == "running":
+                tmpl = self._("The tests for the {image_name} have "
+                              "started {status}")
+            if status == "aborted":
+                tmpl = self._("The tests for the {image_name} have "
+                              "been {status}")
+            if status == "failed":
+                tmpl = self._("The tests for the {image_name} {status}")
+            if status == "success":
+                tmpl = self._("The tests for {image_name} were a {status}")
+
+            if release:
+                image_name = "%s (%s)" % (image_name, release)
+
+            return tmpl.format(image_name=image_name, status=status)
+
+    def handle_old_autocloud_link(self, msg, **config):
+        job_id = msg['msg'].get('job_id')
+        if job_id:
+            template = 'https://apps.fedoraproject.org/autocloud/jobs/%s/output'
+            return template % job_id
+        else:
+            return msg['msg']['image_url']
+
+    def handle_old_autocloud_objects(self,msg, **config):
+        status = msg['msg']['status']
+        return set(['autocloud/image/' + status])
+
+
--- fedmsg_meta_fedora_infrastructure/tests/autocloud.py
+++ fedmsg_meta_fedora_infrastructure/tests/autocloud.py
@@ -289,6 +289,156 @@ class ComposeTestCompleted(Base):
         }
     }
 
+
+
+class LegacyTestImageTestQueued(Base):
+    """ These messages are published when an image is queued for testing in
+    Autocloud app.
+    """
+    expected_title = "autocloud.image.queued"
+    expected_subti = 'Fedora-Cloud-Base is queued for testing'
+    expected_link = 'https://kojipkgs.fedoraproject.org//work/' + \
+        'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2'
+    expected_secondary_icon = "https://apps.fedoraproject.org/img/icons/" + \
+        "fedimg.png"
+    expected_packages = set([])
+    expected_usernames = set([])
+    expected_objects = set(['autocloud/image/queued'])
+    msg = {
+        u'username': u'apache',
+        u'i': 1,
+        u'timestamp': 1379638157.759283,
+        u'msg_id': u'2015-4b5aae66-b713-4c22-bb4a-1277d4402375',
+        u'topic': u'org.fedoraproject.prod.autocloud.image.queued',
+        u'msg': {
+            u'buildid': u'10291410',
+            u'status': u'queued',
+            u'image_url': u'https://kojipkgs.fedoraproject.org//work/'
+            'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2',
+            u'image_name': u'Fedora-Cloud-Base'
+        }
+    }
+
+
+class LegacyTestImageTestRunning(Base):
+    """ These messages are published when tests for an image starts
+    in Autocloud app.
+    """
+    expected_title = "autocloud.image.running"
+    expected_subti = 'The tests for the Fedora-Cloud-Base (rawhide) have started running'
+    expected_link = 'https://kojipkgs.fedoraproject.org//work/' + \
+        'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2'
+    expected_secondary_icon = "https://apps.fedoraproject.org/img/icons/" + \
+        "fedimg.png"
+    expected_packages = set([])
+    expected_usernames = set([])
+    expected_objects = set(['autocloud/image/running'])
+    msg = {
+        u'username': u'apache',
+        u'i': 1,
+        u'timestamp': 1379638157.759283,
+        u'msg_id': u'2015-4b5aae66-b713-4c22-bb4a-1277d4402375',
+        u'topic': u'org.fedoraproject.prod.autocloud.image.running',
+        u'msg': {
+            u'buildid': u'10291410',
+            u'status': u'running',
+            u'image_url': u'https://kojipkgs.fedoraproject.org//work/'
+            'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2',
+            u'image_name': u'Fedora-Cloud-Base',
+            u'release': 'rawhide',
+        }
+    }
+
+
+class LegacyTestImageTestAborted(Base):
+    """ These messages are published when tests for an image starts
+    in Autocloud app.
+    """
+    expected_title = "autocloud.image.aborted"
+    expected_subti = 'The tests for the Fedora-Cloud-Base (rawhide) have been aborted'
+    expected_link = 'https://kojipkgs.fedoraproject.org//work/' + \
+        'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2'
+    expected_secondary_icon = "https://apps.fedoraproject.org/img/icons/" + \
+        "fedimg.png"
+    expected_packages = set([])
+    expected_usernames = set([])
+    expected_objects = set(['autocloud/image/aborted'])
+    msg = {
+        u'username': u'apache',
+        u'i': 1,
+        u'timestamp': 1379638157.759283,
+        u'msg_id': u'2015-4b5aae66-b713-4c22-bb4a-1277d4402375',
+        u'topic': u'org.fedoraproject.prod.autocloud.image.aborted',
+        u'msg': {
+            u'buildid': u'10291410',
+            u'status': u'aborted',
+            u'image_url': u'https://kojipkgs.fedoraproject.org//work/'
+            'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2',
+            u'image_name': u'Fedora-Cloud-Base',
+            u'release': 'rawhide',
+        }
+    }
+
+
+class LegacyTestImageTestFailed(Base):
+    """ These messages are published when tests for an image starts
+    in Autocloud app.
+    """
+    expected_title = "autocloud.image.failed"
+    expected_subti = 'The tests for the Fedora-Cloud-Base (23) failed'
+    expected_link = 'https://apps.fedoraproject.org/autocloud/jobs/412/output'
+    expected_secondary_icon = "https://apps.fedoraproject.org/img/icons/" + \
+        "fedimg.png"
+    expected_packages = set([])
+    expected_usernames = set([])
+    expected_objects = set(['autocloud/image/failed'])
+    msg = {
+        u'username': u'apache',
+        u'i': 1,
+        u'timestamp': 1379638157.759283,
+        u'msg_id': u'2015-4b5aae66-b713-4c22-bb4a-1277d4402375',
+        u'topic': u'org.fedoraproject.prod.autocloud.image.failed',
+        u'msg': {
+            u'buildid': u'10291410',
+            u'status': u'failed',
+            u'job_id': 412,
+            u'image_url': u'https://kojipkgs.fedoraproject.org//work/'
+            'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2',
+            u'image_name': u'Fedora-Cloud-Base',
+            u'release': u'23',
+        }
+    }
+
+
+class LegacyTestImageTestSuccess(Base):
+    """ These messages are published when tests for an image successfully
+    completes in Autocloud app.
+    """
+    expected_title = "autocloud.image.success"
+    expected_subti = 'The tests for Fedora-Cloud-Base were a success'
+    expected_link = 'https://kojipkgs.fedoraproject.org//work/' + \
+        'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2'
+    expected_secondary_icon = "https://apps.fedoraproject.org/img/icons/" + \
+        "fedimg.png"
+    expected_packages = set([])
+    expected_usernames = set([])
+    expected_objects = set(['autocloud/image/success'])
+    msg = {
+        u'username': u'apache',
+        u'i': 1,
+        u'timestamp': 1379638157.759283,
+        u'msg_id': u'2015-4b5aae66-b713-4c22-bb4a-1277d4402375',
+        u'topic': u'org.fedoraproject.prod.autocloud.image.success',
+        u'msg': {
+            u'buildid': u'10291410',
+            u'status': u'success',
+            u'image_url': u'https://kojipkgs.fedoraproject.org//work/'
+            'taskstasks/1410/10291410/Fedora-Cloud-Base-22-20150705.i386.qcow2',
+            u'image_name': u'Fedora-Cloud-Base'
+        }
+    }
+
+
 add_doc(locals())
 
 if __name__ == '__main__':
