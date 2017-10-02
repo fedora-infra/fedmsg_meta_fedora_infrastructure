dcf6ee2ea004f7106a3b851bf2e05e11de2e4d30
Pierre-Yves Chibon <pingou@pingoured.fr>
2017-10-02 11:47:24 +0200

    Release 0.22.0
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ entry_points = {
 
 setup(
     name='fedmsg_meta_fedora_infrastructure',
-    version='0.21.0',
+    version='0.22.0',
     description=
     "fedmsg metadata providers for Fedora Infrastructure's deployment",
     long_description=long_description,
