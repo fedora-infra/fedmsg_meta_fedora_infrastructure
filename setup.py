cc6ba8f317570aa28a269a969e17a4992ac29c25
Stephen Coady <scoady@redhat.com>
2021-03-23 14:57:14 +0000

    Release 0.31.0
    Signed-off-by: Stephen Coady <scoady@redhat.com>

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ entry_points = {
 
 setup(
     name='fedmsg_meta_fedora_infrastructure',
-    version='0.30.0',
+    version='0.31.0',
     description=
     "fedmsg metadata providers for Fedora Infrastructure's deployment",
     long_description=long_description,
